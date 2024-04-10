4dc2602935de07114baaf1bc988f36fd80f4fd8c
JulieSoboleva <ju.soboll@gmail.com>
2024-04-10 09:42:50 +0300

    Update character.test.js

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -1,46 +1,33 @@
-import Bowman from '../bowman';
-import Character from '../character';
-import Daemon from '../daemon';
-import Swordsman from '../swordsman';
-import Undead from '../undead';
-import Zombie from '../zombie';
+import Bowman from '../class/bowman';
+import Character from '../class/character';
+import Daemon from '../class/daemon';
+import Swordsman from '../class/swordsman';
+import Undead from '../class/undead';
+import Zombie from '../class/zombie';
 
 
 test('check levelUp', () => {
     const bowman = new Bowman("Алекс");
     bowman.levelUp();
-
-    expect(bowman.level).toBe(2);
-    expect(bowman.attack).toBe(30);
-    expect(bowman.defence).toBe(30);
+    const result = {
+        attack: 30, defence: 30, health: 100, level: 2, name: 'Алекс', type: 'Bowman'
+    };
+    expect(bowman).toEqual(result);
 });
 
 test('check levelUp for dead man', () => {
     const zombie = new Zombie("Фредди");
     zombie.health = 0;
-    try {
-        zombie.levelUp();
-    } catch (e) {
-        expect(e.message).toBe("Нельзя повысить левел умершего");
-    }
-    
-
+    expect(() => zombie.levelUp()).toThrow();
 });
 
 test('check name length', () => {
-    try {
-        new Daemon("Вольфшлегельштайнхаузенбергедорф");
-    } catch (e) {
-        expect(e.message).toBe("Имя должно иметь от 2 до 10 символов");
-    }
+    expect(() => new Daemon("А")).toThrow();
+
 });
 
 test('check person type', () => {
-    try {
-        new Character("Merlin", "Cook", 10, 50);
-    } catch (e) {
-        expect(e.message).toBe("Не корректный тип персонажа");
-    }
+    expect(() => new Character("Merlin", "Cook", 10, 50)).toThrow();
 });
 
 test('check method damage', () => {
@@ -53,4 +40,17 @@ test('check damage with health less zero', () => {
     const swordsman = new Swordsman("Вова");
     swordsman.damage(120);
     expect(swordsman.health).toBe(0);
+});
+
+test('check creating object', () => {
+    const person = new Character('Петя', 'Undead');
+    const correct = {
+        attack: undefined,
+        defence: undefined,
+        health: 100,
+        level: 1,
+        name: 'Петя',
+        type: 'Undead'
+    }
+    expect(person).toEqual(correct);
 });
